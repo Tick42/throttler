87c75448434711f545fdd9cadd0d512977a19b16
lkonstantinov <lubomir.konstantinov@tick42.com>
2019-04-10 10:41:21 +0300

    - updated deps

--- project.clj
+++ project.clj
@@ -3,9 +3,8 @@
   :url "https://github.com/tick42/throttler"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-RC1" :scope "provided"]
-                 [org.clojure/spec.alpha "0.1.143" :scope "provided"]
-                 [org.clojure/core.async "0.3.465" :scope "provided"]]
+  :dependencies [[org.clojure/clojure "1.10.0" :scope "provided"]
+                 [org.clojure/core.async "0.4.490" :scope "provided"]]
   :profiles {:dev {:dependencies [[criterium "0.4.3"]]
                    :plugins      [[test2junit "1.3.3"]]}}
 
